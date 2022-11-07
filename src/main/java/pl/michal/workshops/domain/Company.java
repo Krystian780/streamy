cc3218d0cfa88a4b0643cf764a74f4ada1a06980
Krystian M <krystian2007@gmail.com>
2022-11-07 17:34:05 +0100

    initial

---
+++ src/main/java/pl/michal/workshops/domain/Company.java
@@ -0,0 +1,27 @@
+package pl.michal.workshops.domain;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Builder
+@Data
+public class Company {
+    private final String name;
+    private final List<User> users;
+
+    public long getUsers2(){
+        return users.stream().count();
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public long getAccounts(){
+        return users.stream().map(User::getAccountsSize).collect(Collectors.summingInt(Long::intValue));
+    }
+
+}
