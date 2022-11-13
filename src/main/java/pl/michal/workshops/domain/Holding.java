4fbf3d7b85447bb7c2bd5a5d1b2f538d8f674593
Krystian780 <61690091+Krystian780@users.noreply.github.com>
2022-11-13 13:24:45 +0100

    'done' (#1)
    * 'done'
    
    * 'corrected'
    
    * 'holding_corrected'

--- src/main/java/pl/michal/workshops/domain/Holding.java
+++ src/main/java/pl/michal/workshops/domain/Holding.java
@@ -17,40 +17,10 @@ public class Holding implements Comparable<Holding> {
     private final String name;
     private final List<Company> companies;
 
-    public boolean isEmpty(){
-        return !companies.isEmpty();
-    }
-
-    public String toLowerCase(){
-        return name.toLowerCase();
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getAmount() {
-        return companies.size();
-    }
-
-    public long getAllUsers(){
-        return companies.stream().map(Company::getUsers2).mapToLong(i -> i).sum();
-    }
-
     public List<String> getAllCompanyNames(){
         List<List<String>> companyNames = new ArrayList<>();
         companyNames.add(companies.stream().map(Company::getName).collect(Collectors.toList()));
         return companyNames.stream().flatMap(List::stream).collect(Collectors.toList());
-    }
-
-    public LinkedList<String> getAllCompanyNames2(){
-        List<List<String>> companyNames = new ArrayList<>();
-        companyNames.add(companies.stream().map(Company::getName).collect(Collectors.toList()));
-        return companyNames.stream().flatMap(List::stream).collect(Collectors.toCollection(LinkedList::new));
-    }
-
-    public long getAllAccountsInACompany(){
-        return companies.stream().map(Company::getAccounts).collect(Collectors.summingInt(Long::intValue));
     }
 
     @Override
